af0160544657b8ab1bc3634c24e8c7a6a8126d6b
Glenn McFetridge <glennmcfetrg@gmail.com>
2022-12-24 23:44:12 +0000

    server url

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://jschatopenai.onrender.com/', {
     method: 'POST',
     headers: { 'Content-type': 'application/json' },
     body: JSON.stringify({ prompt: data.get('prompt') }),
